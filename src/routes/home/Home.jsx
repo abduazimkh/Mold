ec1a1a936e308cead07e3c55cf4a1ea6f9b88c37
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-29 16:35:43 +0500

    not changed

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import classes from "./Home.module.scss";
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={classes.home__wrapper}>
+      <div className={classes.home__first__item}>
+        <div className={classes.home__modal__menu}>
+wdqwd
+        </div>
+
+        <div className={classes.home__modal__swiper}>
+dw
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
