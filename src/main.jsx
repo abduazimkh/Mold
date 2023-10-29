3de3ebdd4f2cd2b9de4da35c4ca6e708384f03a6
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-29 16:34:25 +0500

    brose router connected

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './reset/reset.css';
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 
 )
