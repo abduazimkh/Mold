a2de948ef3a9169dab49c563ca2490a018b9c6e2
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-29 16:37:26 +0500

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import classes from './App.module.scss';
 import Nav from './components/nav/Nav';
+import Home from './routes/home/Home';
+import About from './routes/about/About';
+import Contact from './routes/contact/Contact';
+import Partners from './routes/partners/Partners';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-
-
   return (
-    <>
+    <div className={classes.App__main}>
       <Nav/>
-    </>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/partners' element={<Partners/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </div>
   )
 }
 
