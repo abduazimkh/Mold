05e8a49c2b04b44cbd5b5241159157a885226848
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-11-02 16:00:50 +0500

    hom page card section added

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,10 +1,10 @@
 import classes from "./Home.module.scss";
-
+import { Container } from "../utils/Utils";
 
 const Home = () => {
   return (
     <div className={classes.home__wrapper}>
-      <div className={classes.home__first__item}>
+      {/* <div className={classes.home__first__item}>
         <div className={classes.home__modal__menu}>
 wdqwd
         </div>
@@ -13,7 +13,31 @@ wdqwd
 dw
         </div>
         
+      </div> */}
+
+      <div className={classes.clients}>
+        <h2>Мы предлагаем клиентам следующее</h2>
+
+        <ul className={classes.clients__cards}>
+          <li>
+            <h3>Покупка в рассрочку</h3>
+            <p>Удобная схема онлайн-заказа продукции в MOLD-COMPONENTS.COM</p>
+          </li>
+          <li>
+            <h3>Бесплатная доставка</h3>
+            <p>Качественные продукты и услуги</p>
+          </li>
+          <li>
+            <h3>Бонусная система</h3>
+            <p>Бонусная система</p>
+          </li>
+          <li>
+            <h3>Помощь</h3>
+            <p>Частые вопросы</p>
+          </li>
+        </ul>
       </div>
+
     </div>
   )
 }
