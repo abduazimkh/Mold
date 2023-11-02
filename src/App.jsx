857e0e29bc18d9c8677b79d24cb35f28ed8636e1
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-11-02 15:35:37 +0500

    footer addded

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './routes/about/About';
 import Contact from './routes/contact/Contact';
 import Partners from './routes/partners/Partners';
 import { Routes, Route } from 'react-router-dom';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
       </Routes>
+      <Footer/>
     </div>
   )
 }
