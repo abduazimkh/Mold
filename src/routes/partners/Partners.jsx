f9c8dff72797ec39d57b1f0ea87e74628f4e3bf0
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-11-06 23:20:36 +0500

    patterns page added

--- src/routes/partners/Partners.jsx
+++ src/routes/partners/Partners.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import classes from "./Partners.module.scss";
+import pattersImg from "../../assets/images/patterns.png"
+import { Container } from "../utils/Utils";
+ 
 const Partners = () => {
   return (
-    <div>Partners</div>
+    <Container>
+      <div className={classes.patterns__wrapper}>
+        <h2>Партнеры</h2>
+
+        <div className={classes.patterns__item}>
+          <img src={pattersImg} alt="" />
+          <h3>Идет строительство</h3>
+          
+        </div>
+      </div>
+    </Container>
   )
 }
 
