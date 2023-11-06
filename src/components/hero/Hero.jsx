094e9fa37e294ff3e428e30cbd496ef5f4069222
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-11-06 23:53:59 +0500

    swiper bag

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,15 @@ import classes from "./Hero.module.scss";
 import shop from "../../assets/images/karzinka.svg"
 import nothing from "../../assets/images/nothing.png"
 import { AiOutlineClose } from "react-icons/ai";
-import { useRef } from "react";
+import React, { useRef, useState } from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+// import heroSwiper1 from "../../assets/images/hero1.svg"
+// import heroSwiper2 from "../../assets/images/hero2.svg"
+
 
 const Hero = () => {
   const closeKarzinkaBtn = useRef();
@@ -40,6 +48,29 @@ const Hero = () => {
             <img src={nothing} alt="" />
           </ul>
         </div>
+
+      {/* <div className={classes.swiper}> */}
+
+      {/* <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={heroSwiper1} alt="hero mold picture" /></SwiperSlide>
+        <SwiperSlide><img src={heroSwiper2} alt="hero mold picture" /></SwiperSlide>
+      </Swiper> */}
+
+      {/* </div> */}
+
     </div>
   )
 }
